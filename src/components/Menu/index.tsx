c5095848a99d3eea7fb329116bf9ed6371779d04
jnestormg <nmeneses51@gmail.com>
2024-10-23 17:42:30 -0600

    cambio menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -9,7 +9,7 @@ const Menu = ({ item, addItem }: ItemsProps) => {
     <div className="">
       <button
         onClick={() => addItem(item)}
-        className="border-2 border-teal-900 w-[90%] p-5
+        className="border-2 border-teal-900 w-[100%] p-5
      hover:bg-teal-800 hover:text-white flex justify-between mb-2 rounded-md">
         <h1>{item.name}</h1>
         <h2 className="font-bold">${item.price}</h2>
